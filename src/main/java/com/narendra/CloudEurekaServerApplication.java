154fc03e0082ea5566c8d293ad205f611b8003e2
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-25 12:21:41 +0530

    Eureka update

--- src/main/java/com/narendra/CloudEurekaServerApplication.java
+++ src/main/java/com/narendra/CloudEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.narendra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudEurekaServerApplication {
 
 	public static void main(String[] args) {
