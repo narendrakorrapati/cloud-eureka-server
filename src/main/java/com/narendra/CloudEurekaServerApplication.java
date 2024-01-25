b461b921221edcf9306a8160133c0bf9e2a2d5d1
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-25 10:10:03 +0530

    First commit

---
+++ src/main/java/com/narendra/CloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.narendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaServerApplication.class, args);
+	}
+
+}
